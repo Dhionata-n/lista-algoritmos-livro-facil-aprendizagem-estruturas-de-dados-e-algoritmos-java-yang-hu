28e8c6f756bea8b82f73994da154874071ef2e65
Dhionatan <dhionatan.dvg@gmail.com>
2022-06-27 20:18:07 -0300

    "Outra maneira de classificar"

--- AlgoritmoOrdenacao/src/Main.java
+++ AlgoritmoOrdenacao/src/Main.java
@@ -7,7 +7,12 @@ public class Main {
 
         for (int scor:scores
              ) System.out.print(scor + ", ");
+        System.out.println();
+        int [] scores2 = {200, 10, 25, 89, 60};
+        Scores.sort2(scores2);
 
+        for (int scor:scores2
+        ) System.out.print(scor + ", ");
 
     }
 }
--- AlgoritmoOrdenacao/src/entities/Scores.java
+++ AlgoritmoOrdenacao/src/entities/Scores.java
@@ -20,4 +20,24 @@ public class Scores {
         }
 
     }
+
+    public static void sort2(int [] arrays){ //Segundo metodo para Ordenação
+        for (int i = 0; i < arrays.length-1; i++){ // laço pecorre o vetor até o ultimo valor valido
+            int minIdex = i; //Indice minimo
+            for( int j = i+1; j < arrays.length;j++){ //laço sempre aponta para o proximo valor
+                if(arrays[minIdex]>arrays[j]){ // laço compara se o valor atual é maior que os proximos valores
+                    minIdex = j; // caso positivo minIdex recebe a posiçao do valor maior que o atual
+                }
+
+            }
+            if (i!=minIdex){ // se for encontrado valor maior inicia a permuta
+                int temp = arrays[i]; // varial temporaria que recebe o valor atual
+                arrays[i] = arrays[minIdex];// valor atual é sobrecrito com valor maior
+                arrays[minIdex] = temp;// proximo valor recebe o menor valor e repete o laço ate passar por todos os valores
+
+            }
+
+        }
+
+    }
 }
